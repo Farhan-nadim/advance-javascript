e27104ff3fa03eba26aa8984352e6ff6cb14527c
Farhan-Nadim <mdfarhannadim12@gmail.com>
2021-05-02 21:12:28 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,29 @@
+
+
+//--Truthy--//
+//0
+//"string"  
+// true      
+//[]
+//{}
+
+
+
+//--Falsy--//
+//Nan
+//null
+//undefined
+//0
+//false
+//"" [empty string]
+
+
+
+let name={};
+
+console.log(name);
+if ( name){
+    console.log("condition is true");
+}else {
+    console.log("condition is false")
+}
